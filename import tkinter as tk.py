8493376165cf2039ae4068b07ae0450233eb4832
klarejust <klarejust@yandex.ru>
2026-02-17 14:04:18 +0300

    добавил счётчик

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -3,4 +3,12 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Простой счетчик")
 root.geometry("300x200")
+counter = tk.IntVar(value=0)
 
+def increase ():
+    counter.set(counter.get() + 1)
+
+def decrease ():
+    counter.set(counter.get() - 1)
+
+root.mainloop()
