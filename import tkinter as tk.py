0def84654c3512cb7a6a566113a1d437e471d407
Alegoso <artemyrlov2022@gmail.com>
2026-02-17 14:07:52 +0300

    Дополнил кнопки

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -16,4 +16,12 @@ label = tk.Label(root, textvariable=counter, font=("Arial", 40))
 label.pack(pady=20)
 frame = tk.Frame(root)
 frame.pack()
+btn_increase = tk.Button(frame, text="+", width=5, command=increase)
+btn_increase.grid(row=0, column=0, padx=5)
+
+btn_decrease = tk.Button(frame, text="-", width=5, command=decrease)
+btn_decrease.grid(row=0, column=1, padx=5)
+
+btn_reset = tk.Button(root, text="Сброс", command=reset)
+btn_reset.pack(pady=10)
 root.mainloop()
